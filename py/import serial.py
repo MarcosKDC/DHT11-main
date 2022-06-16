effd355145b742b175cd16b1a3fdfa2a57f6af64
MarcosKDC <marcoskitosdc.com>
2022-06-16 14:48:27 +0200

    modified:   .pio/build/uno/.sconsign39.dblite 	modified:   .pio/build/uno/FrameworkArduino/CDC.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/HardwareSerial.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/HardwareSerial0.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/HardwareSerial1.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/HardwareSerial2.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/HardwareSerial3.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/IPAddress.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/PluggableUSB.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/Print.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/Stream.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/Tone.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/USBCore.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/WInterrupts.c.o 	modified:   .pio/build/uno/FrameworkArduino/WMath.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/WString.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/abi.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/hooks.c.o 	modified:   .pio/build/uno/FrameworkArduino/main.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/new.cpp.o 	modified:   .pio/build/uno/FrameworkArduino/wiring.c.o 	modified:   .pio/build/uno/FrameworkArduino/wiring_analog.c.o 	modified:   .pio/build/uno/FrameworkArduino/wiring_digital.c.o 	modified:   .pio/build/uno/FrameworkArduino/wiring_pulse.S.o 	modified:   .pio/build/uno/FrameworkArduino/wiring_pulse.c.o 	modified:   .pio/build/uno/FrameworkArduino/wiring_shift.c.o 	modified:   .pio/build/uno/firmware.elf 	modified:   .pio/build/uno/firmware.hex 	deleted:    .pio/build/uno/idedata.json 	modified:   .pio/build/uno/lib469/Adafruit Unified Sensor/Adafruit_Sensor.cpp.o 	modified:   .pio/build/uno/lib469/libAdafruit Unified Sensor.a 	modified:   .pio/build/uno/libFrameworkArduino.a 	modified:   .pio/build/uno/libbe5/DHT sensor library/DHT.cpp.o 	modified:   .pio/build/uno/libbe5/DHT sensor library/DHT_U.cpp.o 	modified:   .pio/build/uno/libbe5/libDHT sensor library.a 	modified:   .pio/build/uno/src/main.cpp.o 	modified:   py/import serial.py

--- py/import serial.py
+++ py/import serial.py
@@ -1,7 +1,26 @@
 import serial
-ser = serial.Serial(
+ser1 = serial.Serial(
         # Serial Port to read the data from
         port='COM3',
+ 
+        #Rate at which the information is shared to the communication channel
+        baudrate = 9600,
+   
+        #Applying Parity Checking (none in this case)
+        parity=serial.PARITY_NONE,
+ 
+       # Pattern of Bits to be read
+        stopbits=serial.STOPBITS_ONE,
+     
+        # Total number of bits to be read
+        bytesize=serial.EIGHTBITS,
+ 
+        # Number of serial commands to accept before timing out
+        timeout=1
+)
+ser2 = serial.Serial(
+        # Serial Port to read the data from
+        port='COM10',
  
         #Rate at which the information is shared to the communication channel
         baudrate = 9600,
@@ -24,5 +43,7 @@ counter= 0
 while 1:
        # ser.write("Write counter: %d \n"%(counter))
         counter += 1
-        x=ser.readline()
+        x=ser1.readline()
         print (x)
+        ser2.write (x)
+        
