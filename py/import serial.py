d631d9a4321d9660d50a8dd1131f8275a7082d82
MarcosKDC <marcoskitosdc@gmail.com>
2022-06-28 20:55:47 +0200

    modified:   .pio/build/project.checksum 	modified:   logTemperaturaapet.csv 	modified:   py/import serial.py 	modified:   src/main.cpp

--- py/import serial.py
+++ py/import serial.py
@@ -21,62 +21,6 @@ nombre=nombre+'.csv'
 cabecera=0 #flag para saber si acabó la cabecera
 x=0 #variable para las lecturas
 s="11111111\r\n" #señal de inicio de cabecera
-<<<<<<< HEAD
-s=s.encode("utf-8")     #codificamos en bits para la comparación    
-                       
-input('Wait for Arduino LED and Click Enter')#espera a que el usuario clique enter
-ser1.write(bytes('00000001','utf-8')) #envía la señal de arranque alarduino vía puerto serie 
-print("Señal de arranque enviada")
-
-file1 = open(nombre, 'w') #abre el archivo en modo write (si existe sobreescribe)
-date=datetime.now() #lee la fecha
-dia= date.strftime('%A, %d. %B %Y')#fecha -> dia string
-hora = date.strftime('%H:%M:%S') #fecha -> hora string
-file1.write("Día;")        #escribe el día
-file1.write(dia) 
-file1.close()  
-
-file1 = open(nombre, 'a') #abre el archivo en modo append
-
-file1.write(";Hora de Inicio;")# escribe la hora
-file1.write(hora)
-file1.write('\n')
-file1.close() 
-print("Fecha:   ",dia) #Printea Fecha y hora de inicio
-print("Hora de Inicio:",hora)
-
-while (cabecera==0): #mientras no se active el flag de cabecera
-        x=ser1.readline() #lee el serial y guardalo en x
-        if  x==s : #si x es la señal de final de cabecera
-                cabecera=1 #activa el flag
-                x=ser1.readline() #lee el serial y guardalo en x
-                x=x.decode('utf-8') #conviertelo a string 
-                print("Se ha finalizado el setup, comienza el log")
-                file1 = open(nombre, 'a') #abre el archivo en modo append
-                file1.write(x)   
-                file1.close
-        else: #de lo contrario, printea la cabecera
-                
-                x=x.decode('utf-8') #conviertelo a string 
-                
-                print(x)
-                      
-
-while (cabecera==1): #mientras no pulses q
-        date=datetime.now() #lee la fecha
-        dia= date.strftime('%A, %d. %B %Y')#fecha -> dia string
-        hora = date.strftime('%H:%M:%S') #fecha -> hora string
-
-        if(ser1.in_waiting>0 and os.path.exists(nombre)): #si el archivo existe y acabó la cabecera
-                         file1 = open(nombre, 'a') #abre el archivo en modo append
-                         x=ser1.readline() #lee el serial y guardalo en x
-                         file1.write(hora) #escribe la hora en la primera linea
-                         file1.write(';')             
-                         x=x.decode('utf-8') #convierte x a string
-                         file1.write(x)  #escribe x (datos recibidos de arduino)
-                         file1.write('\n')   #siguiente línea
-                         file1.close()
-=======
 s=s.encode("utf-8")     #codificamos en bits para la comparación
 on=True
 data=[]
@@ -160,7 +104,6 @@ while (True): #mientras no pulses q
         else:
                 ser1.write(bytes('00000001','utf-8'))
                       
->>>>>>> 374d9dcd615409e323a51a9d26767089e2041cb5
                         
                                
                                         
--- src/main.cpp
+++ src/main.cpp
@@ -71,54 +71,13 @@ void setup() {
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
   Serial.println(F("11111111")); //Manda la señal de arranque
-<<<<<<< HEAD
-  Serial.println(F("Hora [HH:MM:SS];Temperatura[ºC];Humedad[%];OK?[I/O]"));
-=======
   Serial.println(F("Hora [HH:MM:SS]; Temperatura[Celsius];  Humedad[%];  OK?[I/O]"));
->>>>>>> 374d9dcd615409e323a51a9d26767089e2041cb5
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
   
 }
 
 void loop() {
-<<<<<<< HEAD
-  digitalWrite(LED_BUILTIN, HIGH); 
-  // Delay between measurements.
-  delay(delayMS);
-  digitalWrite(LED_BUILTIN, LOW);
-  // Get temperature and humidity event
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  float temp=0;
-  temp= event.temperature;
-  dht.humidity().getEvent(&event);
-  float humedad=0;
-  humedad= event.relative_humidity;
-  
-  if (isnan(event.temperature)||isnan (event.relative_humidity)) {// If can't read, display error
-    Serial.print(F("ERROR"));
-    Serial.print(F(";"));
-    Serial.print(F("ERROR"));
-    Serial.print(F(";"));
-    Serial.println(F("0"));
-  }
-  else {// Else, print values
-    Serial.print(temp);
-    Serial.print(F("; "));
-    Serial.print(humedad);
-    Serial.print(F(";"));
-    if(temp>27 || temp<17  || humedad>70 || event.relative_humidity<50 ) //If values are out of legal range, set alarm
-    {
-     Serial.println(F("1"));
-    }
-    else
-    {
-      Serial.println(F("0"));
-    }
-  }
-}
-=======
       // Delay between measurements.
       if(Serial.available()>0){
       digitalWrite(LED_BUILTIN, HIGH);  
@@ -161,4 +120,3 @@ void loop() {
         }
       }
   }
->>>>>>> 374d9dcd615409e323a51a9d26767089e2041cb5
